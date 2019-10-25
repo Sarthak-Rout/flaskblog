3b6bdceba01bf313e512f4dc649c0aa0cf67c734
Sarthak-Rout <35285088+Sarthak-Rout@users.noreply.github.com>
2019-10-26 01:55:32 +0530

    create database

--- flaskblog.py
+++ flaskblog.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
